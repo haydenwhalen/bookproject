f9a5c7907c5f869d63d877e649ed2f0ffea375fc
haydenwhalen <153124475+haydenwhalen@users.noreply.github.com>
2025-03-21 15:25:04 -0600

    Your commit message for phase02 updates

--- BACKEND/bookproject.API/obj/Debug/net8.0/BookProject.API.AssemblyInfo.cs
+++ BACKEND/bookproject.API/obj/Debug/net8.0/BookProject.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookProject.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+053a5a8319bb108d74ec85ff630af5207e42aa2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b3bf41837eea750f492a599e44a1c90f70a3639")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookProject.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookProject.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
